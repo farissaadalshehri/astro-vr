334c84a24929397c68f387f6a26a4c02abaa58ae
Faris AlShehri <faris@shehri.me>
2018-10-19 20:43:32 +0300

    linked trigger to open door

---
+++ Assets/TriggerDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour {
+
+    public Animator anim;
+    public float angle;
+    bool opened = false;
+    Valve.VR.InteractionSystem.CircularDrive reader;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        reader = gameObject.GetComponent<Valve.VR.InteractionSystem.CircularDrive>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (opened)
+            return;
+        angle = reader.outAngle;
+
+        if (angle > 270 )
+        {
+            anim.SetTrigger("open");
+            opened = true;
+        }
+	}
+}
