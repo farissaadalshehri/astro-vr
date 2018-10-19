274b52069168fee5e004699eb801c3674f15843a
Faris AlShehri <faris@shehri.me>
2018-10-19 23:01:57 +0300

    Revert "Revert "Added valve switch to trigger the door to open and repositioned teleportation points""
    This reverts commit 1322a9e1d88f614c3dc060658cb39e2dbc9b737d.

--- Assets/TriggerDoor.cs
+++ Assets/TriggerDoor.cs
@@ -5,29 +5,9 @@ using UnityEngine;
 public class TriggerDoor : MonoBehaviour {
 
     public Animator anim;
-    public float angle;
-    bool opened = false;
-    Valve.VR.InteractionSystem.CircularDrive reader;
 
-    // Use this for initialization
-    void Start ()
+    public void openDoor ()
     {
-
-        reader = gameObject.GetComponent<Valve.VR.InteractionSystem.CircularDrive>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-
-        if (opened)
-            return;
-        angle = reader.outAngle;
-
-        if (angle > 270 )
-        {
-            anim.SetTrigger("open");
-            opened = true;
-        }
-	}
+        anim.SetTrigger("open");
+    }
 }
---
+++ Assets/TriggerSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSwitch : MonoBehaviour {
+
+    public Animator anim;
+    public float angle;
+    bool opened = false;
+    Valve.VR.InteractionSystem.CircularDrive reader;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        reader = gameObject.GetComponent<Valve.VR.InteractionSystem.CircularDrive>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (opened)
+            return;
+        angle = reader.outAngle;
+
+        if (angle > 400 )
+        {
+            anim.SetTrigger("switch");
+            opened = true;
+        }
+	}
+}
