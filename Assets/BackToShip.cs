adec91df17240eb797e5c4ec5f99c60a74d2fb5e
Faris AlShehri <faris@shehri.me>
2018-10-20 05:25:04 +0300

    Finished Earth Scene_01

---
+++ Assets/BackToShip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToShip : MonoBehaviour {
+
+    public Transform location;
+    public GameObject menu;
+
+	public void Teleport ()
+    {
+        menu.SetActive(true);
+        transform.position = location.position;
+        transform.rotation = location.rotation;
+    }
+}
---
+++ Assets/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour {
+
+    public string sceneName;
+
+    public void Load ()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
